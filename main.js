f26e89dcfc41e6ba94669fc6eb98bfb3544b28b3
Elijah Prince <elijahprince73@gmail.com>
2017-08-27 14:52:08 -0700

    accordian

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".title1").click(function () {
+    $(".content1").toggleClass("accordian-slider")
+  })
+  $(".title2").click(function () {
+    $(".content2").toggleClass("accordian-slider")
+  })
+  $(".title3").click(function () {
+    $(".content3").toggleClass("accordian-slider")
+  })
+})
